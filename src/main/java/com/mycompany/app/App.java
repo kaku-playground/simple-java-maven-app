7ea62a932d947f739954a44f72acc9de5b6c611b
Guo Yu <kakuyou@example.com>
2020-09-08 00:11:02 +0800

    1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello Jenkins Pipeline as Code.0!";
 
     public App() {}
 
